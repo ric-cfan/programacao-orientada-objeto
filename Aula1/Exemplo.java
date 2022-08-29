3b6bc162666ad44450aea4e4dcf88d24c0709263
ric-cfan <ric_cfan@hotmail.com>
2022-08-29 19:10:49 -0300

    Hello world !

---
+++ Aula1/Exemplo.java
@@ -0,0 +1,10 @@
+public class Exemplo{
+	public static void main(String[] args){
+
+		System.out.println("Hello World!");
+
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+	}
+}
---
+++ Aula1/Exemplo2.java
@@ -0,0 +1,9 @@
+public class Exemplo2{
+	public static void main(String[] args){
+
+		System.out.println("\tRicardo\n\tAndrade");
+
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+	}
+}
